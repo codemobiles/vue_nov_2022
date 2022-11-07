5d9b838fb3fbde6bebb74c21912e205ddaf59f03
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-07 14:09:34 +0700

    1234

--- rvpPOS/frontend/src/components/core/Content.vue
+++ rvpPOS/frontend/src/components/core/Content.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>CodeMobiles</div>
+  <div
+    :style="{
+      padding: '24px',
+      height: '100%',
+      background: '#fff',
+      display: 'flex',
+      justifyContent: 'center',
+    }"
+  >
+    <router-view />
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
 
-<style></style>
+export default defineComponent({
+  components: {},
+});
+</script>
