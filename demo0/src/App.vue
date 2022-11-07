566e183fb00a6af842be721c5d414057618c57c4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-07 10:22:22 +0700

    1234

--- demo0/src/App.vue
+++ demo0/src/App.vue
@@ -9,16 +9,18 @@
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
+
 export default {
   setup(props) {
     const tmp1: number = 1000;
     const tmp2: string = "Lek";
     const tmp3 = "VueJS";
-    let count1 = 0;
+    let count1 = ref(0);
 
     function handleClickMe() {
-      count1 = count1 + 1;
-      console.log("count1: " + count1.toString());
+      count1.value = count1.value + 1;
+      console.log("count1: " + count1.value.toString());
     }
 
     return { tmp1, tmp2, count1, handleClickMe };
