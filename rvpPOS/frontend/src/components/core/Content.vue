9642a2b4de3b34da273be646de35a7d213654308
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-07 14:14:27 +0700

    1234

--- rvpPOS/frontend/src/components/core/Content.vue
+++ rvpPOS/frontend/src/components/core/Content.vue
@@ -1,5 +1,13 @@
 <template>
-  <div style="display: flex; flex-direction: column">
+  <div
+    style="
+      display: flex;
+      flex-direction: column;
+      background-color: yellow;
+      height: 300px;
+      width: 300px;
+    "
+  >
     <!-- <RouterView /> -->
     <div>111</div>
     <div>222</div>
