d3c332305f6f8b2146ba946d1465c46b65f068bf
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-07 14:05:26 +0700

    1234

--- rvpPOS/frontend/src/App.vue
+++ rvpPOS/frontend/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <a-layout style="height: 100%">
     <Menu />
+    <a-layout>
+      <Header />
+      <a-layout-content>
+        <Content />
+      </a-layout-content>
+    </a-layout>
   </a-layout>
 </template>
 
 <script lang="ts">
+import Menu from "@/components/core/Menu.vue";
 export default {
+  components: { Menu },
   setup() {},
 };
 </script>
