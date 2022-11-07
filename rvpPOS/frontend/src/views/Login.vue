0d34193747a4c3e7f385e198fb91926ac1d1d611
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-07 13:33:16 +0700

    1234

--- rvpPOS/frontend/src/views/Login.vue
+++ rvpPOS/frontend/src/views/Login.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Login</div>
+  <div>
+    <h1>Login</h1>
+    <form>
+      <input type="text" placeholder="Username" v-model="user.username" />
+      <br />
+      <input type="password" placeholder="Password" v-model="user.password" />
+      <br />
+      <span>{{ user }}</span
+      ><br />
+      <button>Login</button>
+    </form>
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { reactive } from "vue";
+export default {
+  setup() {
+    const user = reactive({ username: "admin", password: "5555" });
+    return { user };
+  },
+};
 </script>
 
 <style></style>
