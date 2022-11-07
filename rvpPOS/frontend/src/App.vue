a497e3d0bfe57e9d86dc9ceef0153deb74d2d304
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-07 11:56:31 +0700

    1234

--- rvpPOS/frontend/src/App.vue
+++ rvpPOS/frontend/src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <div>
-    <a-button type="primary">Primary Button</a-button>
-    <a-button>Default Button</a-button>
-    <a-button type="dashed">Dashed Button</a-button>
-    <a-button type="text">Text Button</a-button>
-    <a-button type="link">Link Button</a-button>
-
-    <div class="border-dashed border-2 border-red-600 rounded-t-lg">Lek</div>
-  </div>
+ <div>
+  
+ </div>
 </template>
 
 <script lang="ts">
