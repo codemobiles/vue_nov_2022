5b804145fdc81db531e943b11d87e1320fdf565a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-08 13:50:21 +0700

    1234

--- rvpPOS/backend/src/controller/UserController.ts
+++ rvpPOS/backend/src/controller/UserController.ts
@@ -6,6 +6,16 @@ export class UserController {
   private userRepository = AppDataSource.getMongoRepository(Users);
 
   register(request: TypedBodyRequest<Users>, response, next) {
-    return { message: "hey" };
+    try {
+      req.body.created = savedValue(req.body.created, new Date());
+      req.body.level = savedValue(req.body.level, "normal");
+      req.body.__v = savedValue(req.body.__v, 0);
+
+      req.body.password = await bcrypt.hash(req.body.password, 8);
+      const doc = await this.userRepository.save(req.body);
+      return { result: "ok", message: doc };
+    } catch (e) {
+      return { result: "nok", message: "invalid data" };
+    }
   }
 }
