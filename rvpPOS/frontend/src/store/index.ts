1c27b035e63e740147a58cde55d8e4d41c455de3
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-09 09:10:01 +0700

    1234

--- rvpPOS/frontend/src/store/index.ts
+++ rvpPOS/frontend/src/store/index.ts
@@ -1,9 +1,11 @@
+import api from "@/services/api";
 import { createStore } from "vuex";
 
 export default createStore({
   actions: {
-    async doLogin({ commit, dispatch }, user) {
-      alert(JSON.stringify(user));
+    async doLogin({ commit, dispatch }, { username, password }) {
+      let result = await api.login({ username, password });
+      alert(JSON.stringify(result));
     },
   },
 });
