8a9936d5cf6aa2b4bdb9354cfb08b0786ad34383
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-10 10:22:12 +0700

    1234

--- rvpPOS/frontend/src/views/Payment.vue
+++ rvpPOS/frontend/src/views/Payment.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>CodeMobiles</div>
+  <div class="flex flex-col pl-7">
+    <h1>Payment</h1>
+    <span
+      >Total: {{ totalNumber }} / Qty: {{ JSON.parse(order_list).length }}</span
+    >
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+export default {
+  props: ["totalNumber", "order_list"],
+};
 </script>
 
 <style></style>
